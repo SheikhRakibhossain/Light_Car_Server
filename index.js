16d96aa8dc84dc139b8a774baf7910e8d9e73f3f
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-16 18:49:53 +0600

    service specific api created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000 ;
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middle ware
 app.use(cors());
@@ -30,11 +30,20 @@ async function run() {
     const serviceCollection = client.db("car").collection("services")
 
     app.get('/services', async(req, res)=>{
-
       const cursor = serviceCollection.find()
       const result = await cursor.toArray()
       res.send(result);
 
+    })
+    app.get('/services/:id',async(req, res)=>{
+
+      const id = req.params.id;
+      const query ={ _id: new ObjectId(id)}
+      const options = {
+        projection: { title: 1, img: 1, service_id: 1, price: 1, },
+      };
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
