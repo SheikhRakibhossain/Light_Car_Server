c9eaa4892d2ecf552a801200e9da6b21d588e826
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-18 20:33:24 +0600

    decoded option has added in jwt

--- index.js
+++ index.js
@@ -21,6 +21,29 @@ const client = new MongoClient(uri, {
   },
 });
 
+//token verify function for booking
+const verifyjwt =(req, res, next)=>{
+  // console.log('jwt hitting');
+  // console.log(req.headers.authorization);
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    res.status(401).send({error:true, message:"unauthorization access"})
+  }
+
+  const token = authorization.split(' ')[1];
+  console.log('token inside backend jwt verify', token);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded)=>{
+    if(error){
+      res.status(403).send({error:true, message:'unauthorize access'})
+    };
+    req.decoded = decoded;
+    next();
+  })
+
+
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -28,10 +51,10 @@ async function run() {
 
     const serviceCollection = client.db("car").collection("services");
     const checkoutCollection = client.db("car").collection("checkout");
+
 // token api for valid auth
 app.post('/jwt',(req,res)=>{
-
-  const user = req.body;
+  const user = req.body;  
   console.log(user);
   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
   res.send({token});
@@ -64,8 +87,10 @@ app.post('/jwt',(req,res)=>{
     });
 
     //get some booking data from checkout
-    app.get("/checkout", async (req, res) => {
-      // console.log(req.query.email);
+    app.get("/checkout", verifyjwt , async (req, res) => {
+      // console.log(req.headers.authorization);
+      const decoded = req.decoded;
+      console.log('come back decoded', decoded)
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
