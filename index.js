ccba242ceb2f959ced96d923251a352e28436333
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-18 15:32:58 +0600

    jwt api tested

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+var jwt = require('jsonwebtoken');
 
 //middle ware
 app.use(cors());
@@ -27,7 +28,18 @@ async function run() {
 
     const serviceCollection = client.db("car").collection("services");
     const checkoutCollection = client.db("car").collection("checkout");
+// token api for valid auth
+app.post('/jwt',(req,res)=>{
 
+  const user = req.body;
+  console.log(user);
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+  res.send({token});
+
+})
+
+
+//service api for cleint view
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
